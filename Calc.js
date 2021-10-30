f88b92cb216c84e9ab565661affff890785f7f0c
MamonovSPb <93142072+MamonovSPb@users.noreply.github.com>
2021-10-30 17:58:59 +0300

    Add files via upload

--- Calc.js
+++ Calc.js
@@ -1,22 +1,24 @@
-function calc(a,b,c){
-    if(a=="sum"){
-        return (b+c);
-    } else if(a=="sub"){
-        return (b-c);
-    }else if(a=="divide"){
-        return (b/c);
-    }else if (a=="multip"){
-        return (b*c);
-    }else if(a=="rem"){
-        return (b%c);
-    }else if (a=="exp"){
-        return (b**c)
-    } else if(a!=="sum" || a!=="sub" || a!=="divide" || a!=="multip" || a!=="rem" || a!=="exp"){
-        return alert("unknown operation");
-    }else if(b==""||c=="");{
+function calc(a,b,c) {
+    if (b == "" || c == "") {
         return alert("Error");
+    } else if (a == "sum") {
+        return (b + c);
+    } else if (a == "sub") {
+        return (b - c);
+    } else if (a == "divide") {
+        return (b / c);
+    } else if (a == "multip") {
+        return (b * c);
+    } else if (a == "rem") {
+        return (b % c);
+    } else if (a == "exp") {
+        return (b ** c);
+    } else if (a !== "sum" || a !== "sub" || a !== "divide" || a !== "multip" || a !== "rem" || a !== "exp") {
+        return alert("unknown operation");
     }
 }
+
+
 var a=prompt("Арифметическое действие");
 var b=+prompt("Число 1","");
 var c=+prompt("Число 2","");
