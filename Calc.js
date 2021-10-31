a1fdd0d7ef130be2a8beb564d1e028ee9323d408
MamonovSPb <93142072+MamonovSPb@users.noreply.github.com>
2021-10-31 12:26:44 +0300

    Add files via upload

--- Calc.js
+++ Calc.js
@@ -1,22 +1,24 @@
 function calc(a,b,c) {
-    if (b == "" || c == "") {
-        return alert("Error");
-    } else if (a == "sum") {
-        return (b + c);
-    } else if (a == "sub") {
-        return (b - c);
-    } else if (a == "divide") {
-        return (b / c);
-    } else if (a == "multip") {
-        return (b * c);
-    } else if (a == "rem") {
-        return (b % c);
-    } else if (a == "exp") {
-        return (b ** c);
+    if (b == "" || c == ""){
+        return String("Error");
+    }else if(isNaN(b) || isNaN(c)){
+        return String("Error");
+    } else if(a=="sum"){
+        return (b+c);
+    } else if(a=="sub"){
+        return (b-c);
+    }else if(a=="divide"){
+        return (b/c);
+    }else if (a=="multip"){
+        return (b*c);
+    }else if(a=="rem"){
+        return (b%c);
+    }else if (a=="exp"){
+        return (b**c);
     } else {
-        return alert("unknown operation");
+        return String("unknown operation");
     }
-}
+    }
 
 
 var a=prompt("Арифметическое действие");
@@ -26,7 +28,6 @@ var c=+prompt("Число 2","");
 
 let result=calc(a,b,c);
 
-if(isFinite(result)){alert(result);
-}else{alert("Error");
-}
+alert(result);
+
 
